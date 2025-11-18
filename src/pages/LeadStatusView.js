8bec47dcda72bd0c019e68d4aadb84ffdfc9e869
sumairahkanwal13 <sumairahkanwal33@gmail.com>
2025-11-18 14:02:23 +0500

    chors: sales agent filter issue.

--- src/pages/LeadStatusView.js
+++ src/pages/LeadStatusView.js
@@ -22,9 +22,18 @@ export default function LeadStatusView() {
   // Apply all filters in sequence
   let filteredLeads = Lead.filter((lead) => lead.status === status);
 
-  if (agentFilter !== "All") {
-    filteredLeads = filteredLeads.filter((lead) => lead.salesAgent?._id === agentFilter);
-  }
+  //if (agentFilter !== "All") {
+   // filteredLeads = filteredLeads.filter((lead) => lead.salesAgent?._id === agentFilter);
+ // }
+
+ if (agentFilter !== "All") {
+    filteredLeads = filteredLeads.filter((lead) => {
+        return lead.salesAgent && lead.salesAgent._id === agentFilter;
+    });
+}
+
+
+
 
   if (priorityFilter !== "All") {
     filteredLeads = filteredLeads.filter((lead) => lead.priority === priorityFilter);

e6aafc3637b40467fba27f733260085b37dcba43
sumairahkanwal13 <sumairahkanwal33@gmail.com>
2025-11-19 18:58:06 +0500

    Chors: fixed comment issue.

--- src/pages/LeadDetails.js
+++ src/pages/LeadDetails.js
@@ -42,9 +42,7 @@ export default function LeadDetails() {
 
   return (
       <div className="flex-grow-1 px-4">
-        <h2 className="text-center mb-4 mt-4">Lead Management: {leads.name}</h2>
-
-        
+        <h2 className="text-center mb-4 mt-4">Lead Management: {Lead.name}</h2>
         <div className="mb-4">
           <h4 className="mb-3">Lead Details</h4>
           {Lead && (
